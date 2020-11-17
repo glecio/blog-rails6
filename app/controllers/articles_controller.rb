ef16d829c3a5464238b0d818e9bae39ebb66d73d
Glecio Santos <glecio12@gmail.com>
2020-11-16 22:13:25 -0300

    User self destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,6 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(article_params)
-        byebug
         @article.user = current_user
         if @article.save
             flash[:notice] = "Article saved sucessfully"
